3c962f308a5282951146636ec7d5aeb4d38f2212
NayLin1590 <naylintbk15@gmail.com>
2021-10-11 22:53:25 +0630

    JWT and bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "api/auth/login", to: "users#login"
+  get "api/auth/auto_login", to: "users#auto_login"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# user = User.create(name: "admin1",email: "admin1@gmail.com", password: "admin1",profile: "admin1.png", role:0 , create_user_id: 1,updated_user_id: 1)
